d8e8509eb5b4753a37179cf7039eae4cc4df6aee
Nothing <jamoladdin0408@gmail.com>
2023-07-19 15:49:33 +0500

    Commit for test

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("you can check");
+    }
+}
